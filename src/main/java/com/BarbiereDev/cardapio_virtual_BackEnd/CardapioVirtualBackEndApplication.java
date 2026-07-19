ee2c298bb03f785c87657791caadcc011495130f
Murilo-Barbiere <murilobarbiere@gmail.com>
2026-07-19 15:20:47 -0400

    init

---
+++ src/main/java/com/BarbiereDev/cardapio_virtual_BackEnd/CardapioVirtualBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.BarbiereDev.cardapio_virtual_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioVirtualBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioVirtualBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BarbiereDev/cardapio_virtual_BackEnd/CardapioVirtualBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarbiereDev.cardapio_virtual_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioVirtualBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
